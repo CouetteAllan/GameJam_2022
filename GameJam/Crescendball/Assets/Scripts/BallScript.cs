d1128add83e476afca27c6c03c95444cc34f31d1
COUETTE Allan <59956060+SkyLaugh987@users.noreply.github.com>
2022-01-24 16:41:29 +0100

    ballBounce

--- GameJam/Crescendball/Assets/Scripts/BallScript.cs
+++ GameJam/Crescendball/Assets/Scripts/BallScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-    [SerializeField] GameObject ball;
-    public float force;
-    public float speed;
-    int score;
-    Rigidbody2D rb;
-    Vector2 direction;
-    Vector2 dir = new Vector2(1, -1);
+    [SerializeField] GameObject         ball;
+    public float                        force;
+    public float                        speed;
+    int                                 score;
+    Rigidbody2D                         rb;
+    Vector2                             direction;
+    Vector2                             dir = new Vector2(1, -1);
+    Vector2                             lastGoodVel;
     [SerializeField] List<GameObject> wall = new List<GameObject>();
 
     // Start is called before the first frame update
@@ -23,24 +24,32 @@ public class BallScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(rb.velocity.x != 0 && rb.velocity.y != 0)
+        {
+            lastGoodVel = rb.velocity;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {      
         if ( other.gameObject.layer == 6)
         {
+            direction = rb.velocity.normalized;
             Transform transform = other.gameObject.GetComponent<Transform>();
+
+
             if (transform.localScale.x > transform.localScale.y)
             {
-
+                rb.velocity = new Vector2(lastGoodVel.x, -lastGoodVel.y);
             }
+            if (transform.localScale.y > transform.localScale.x)
+            {
+                rb.velocity = new Vector2(-lastGoodVel.x, lastGoodVel.y);
+            }
+
             score = GameManager.Instance.GetScore();
             GameManager.Instance.SetScore(score += 100);
-            //speed = force * score;
-            direction = rb.velocity.normalized;
-            //rb.velocity *= direction * speed / 100;
-            Debug.Log("bite");
+            speed = force * score;
         }
 
         PlayerScript player = other.gameObject.GetComponent<PlayerScript>();
