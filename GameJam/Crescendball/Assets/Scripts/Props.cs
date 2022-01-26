03587c61bcba383804637461e4a2f2f09057ce67
oct0punk <55735176+CouetteAllan@users.noreply.github.com>
2022-01-26 13:43:18 +0100

    props particles

--- GameJam/Crescendball/Assets/Scripts/Props.cs
+++ GameJam/Crescendball/Assets/Scripts/Props.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Props : MonoBehaviour
 {
-
+    private ParticleSystem particles;
 
 
     void Start()
     {
-        
+        particles = GetComponentInChildren<ParticleSystem>();
     }
     void Update()
     {
@@ -21,8 +21,9 @@ public class Props : MonoBehaviour
         BallScript ball = collision.gameObject.GetComponent<BallScript>();
         if(ball.multiplier >= 10)
         {
+            particles.Play();
             PopUpScore.Create(this.transform.position += Vector3.up * 1.2f, 400 * (int)ball.multiplier);
-            Destroy(gameObject);
+            GetComponentInChildren<SpriteRenderer>().enabled = false;
         }
     }
     
